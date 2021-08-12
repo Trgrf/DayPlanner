8ff438b254247216ee5440cfa6c2239c4f59b8fe
Theo <trgrf1@gmail.com>
2021-08-12 15:09:50 -0400

    saving changes

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// create our document ready function to make sure nothing runs before we load the page
+$(document).ready(function () {
+
+// function to listen for click events on page
+  
+    // grab the saved event options
+        // create variables to save user input and time 
+
+    // save to localStorage
+
+    // alert the user that items have been saved to localStorage.
+
+    // create a timeOut value that removes the alert
+
+
+
+
+// create time updater function()
+    // get the current time 
+
+    // create our loop to go over all the time blocks
+    
+    // if else statment to
+        //check to see if we have passed our time
+            // check out removeClass and addClass
+        // check to see if current hour matches physical hour
+
+        // check to see if the time is in the future
+
+
+
+
+
+// Retrieve localStorage and render items to the correct rows
+
+
+
+
+
+
+
+})
