9aa143871e2bf39078a8955070593cbc2132dbb1
Theo <trgrf1@gmail.com>
2021-08-13 16:12:12 -0400

    saving changes to my html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,54 @@
-var currentDayEl = document.getElementById('currentDay')
 var hourEl = document.getElementById('hour-9');
 var userInputEl = document.querySelector('.userinput');
 
+
 // create our document ready function to make sure nothing runs before we load the page
 $(document).ready(function () {
-
+    $('.save-button').on('click', infoEntered)
 // function to listen for click events on page
-function infoEntered(event) {
-    // grab the saved event options
-        // create variables to save user input and time 
-
-    var userData = userInputEl.val().trim();
-    var time = hourEl.val().trim();
-
+function infoEntered() {
     
-    // save to localStorage
-    localStorage.setItem('Todo', userData);
-    localStorage.setItem('time', time);
-
-    // alert the user that items have been saved to localStorage.
-    var Hey = setTimeout(function() { alert("Your task has been saved!"); }, 3000);
+    var task = $(this).siblings('.user-input').val().trim();
+    var time = $(this).parent().attr('id');
     
-    // create a timeOut value that removes the alert
-    endAlert();
+    localStorage.setItem(time, task);
 }
 
-
-function endAlert () {
-    clearTimeout(Hey);
+function hourColorize() {
+    var currentHour = moment().hours()
+    console.log(currentHour);
+    $('.time-block').each(function (){
+        var rowHour = parseInt($(this).attr('id').split('-')[1]);
+        if (rowHour < currentHour) {
+            $(this).children('.user-input').addClass('past')
+        }
+        else if (rowHour === currentHour) {
+            $(this).children('.user-input').addClass('present');
+            $(this).children('.user-input').removeClass('past');
+        }
+        else {
+            $(this).children('.user-input').addClass('future')
+            $(this).children('.user-input').removeClass('present')
+            $(this).children('.user-input').removeClass('past')
+        }
+        console.log(rowHour);
+    })
 }
+hourColorize();
 
-
-// create time updater function()
-function currentTime() {
-    var update = moment().format('MMM DD YYYY [at] hh:mm:ss a');
-    currentDayEl.text(update);
-
-
-
-    // get the current time 
-
-    // create our loop to go over all the time blocks
-    
-    // if else statment to
-        //check to see if we have passed our time
-            // check out removeClass and addClass
-        // check to see if current hour matches physical hour
-
-        // check to see if the time is in the future
-
-
-}
-setInterval (currentTime, 1000);
-
-
-// Retrieve localStorage and render items to the correct rows
+$('#hour-9 .user-input').val(localStorage.getItem('hour-9'));
+$('#hour-10 .user-input').val(localStorage.getItem('hour-10'));
+$('#hour-11 .user-input').val(localStorage.getItem('hour-11'));
+$('#hour-12 .user-input').val(localStorage.getItem('hour-12'));
+$('#hour-13 .user-input').val(localStorage.getItem('hour-13'));
+$('#hour-14 .user-input').val(localStorage.getItem('hour-14'));
+$('#hour-15 .user-input').val(localStorage.getItem('hour-15'));
+$('#hour-16 .user-input').val(localStorage.getItem('hour-16'));
+$('#hour-17 .user-input').val(localStorage.getItem('hour-17'));
 
 
 
-
-userInputEl.on('click', infoEntered)
-
+var update = moment().format('MMM DD YYYY');
+$('#currentDay').text(update);
 
 })
